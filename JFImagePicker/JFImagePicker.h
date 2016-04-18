2b2cc8010199f4ad21c646091ab98f2ab79fc7ac
王义川 <2978439075@qq.com>
2016-04-18 17:35:33 +0800

    增加carthage支持

---
+++ JFImagePicker/JFImagePicker.h
@@ -0,0 +1,27 @@
+//
+//  JFImagePicker.h
+//  JFImagePicker
+//
+//  Created by 王义川 on 16/4/18.
+//  Copyright © 2016年 Johnil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JFImagePicker.
+FOUNDATION_EXPORT double JFImagePickerVersionNumber;
+
+//! Project version string for JFImagePicker.
+FOUNDATION_EXPORT const unsigned char JFImagePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JFImagePicker/PublicHeader.h>
+
+
+#import "JFAssetHelper.h"
+#import "JFImageCollectionViewController.h"
+#import "JFImageGroupTableViewController.h"
+#import "JFImageManager.h"
+#import "JFImagePickerController.h"
+#import "JFImagePickerViewCell.h"
+#import "JFPhotoBrowserViewController.h"
+#import "JFPhotoView.h"
